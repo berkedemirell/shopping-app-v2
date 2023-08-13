70baaf1c900564d5958c64d1793b5f4aa33c4e4b
Berke Demirel <dberke00@gmail.com>
2023-08-13 16:52:44 +0300

    detail

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -19,17 +19,18 @@ const Detail = () => {
 
 
   return (
-    <div>
+    <div className="flex p-10 flex-row items-center justify-center gap-12 font-rem bg-gradient-to-r from-purple-100 to-purple-200">
         <div>
-            <img src={single.thumbnail} alt=""/>
+            <img src={single.thumbnail} alt="" className="w-success-w rounded-full"/>
         </div>
-        <div>
-            <p>{single.brand}</p>
-            <p>{single.title}</p>
-            <p>{single.description}</p>
-            <p>${single.price}</p>
-            <p>{single.rating}/5</p>
-            <p>{single.category}</p>
+        <div className="w-cart flex flex-col gap-4">
+            <p><span className="uppercase font-bold">Brand: </span>{single.brand}</p>
+            <p className="capitalize"><span className="uppercase font-bold">Category: </span>{single.category}</p>
+            <p><span className="uppercase font-bold">Product: </span>{single.title}</p>
+            <p><span className="uppercase font-bold">Description: </span>{single.description}</p>
+            <p><span className="uppercase font-bold">Price: </span>${single.price}</p>
+            <p><span className="uppercase font-bold">Rate: </span>{single.rating}/5</p>
+            <p><span className="uppercase font-bold">Stock: </span>{single.stock}</p>
         </div>
     </div>
   )
