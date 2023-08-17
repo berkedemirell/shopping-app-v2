cd64240cec52adee5c97ceda50b8201b7d5b3a8b
Berke Demirel <dberke00@gmail.com>
2023-08-17 20:24:50 +0300

    revision51

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -2,7 +2,7 @@
 const Progress = () => {
   return (
     <div className="h-screen w-screen bg-indigo-100 p-24 font-rem text-center">
-        <div className="load">
+        <div className="load2">
         </div>
         <div className="mt-24">
             <p className="uppercase text-sm text-indigo-500 font-bold">Payment Processing Please Wait</p>
