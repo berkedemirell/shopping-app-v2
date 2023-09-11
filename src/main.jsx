33a45e8c5eea145be6030a42c9581a4e7bfc2321
Berke Demirel <dberke00@gmail.com>
2023-09-11 16:05:02 +0300

    rev-analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { DataContextProvider } from './context/DataContext.jsx'
+import {Analytics} from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <DataContextProvider>
       <App />
+      <Analytics/>
     </DataContextProvider>
   </React.StrictMode>,
 )
